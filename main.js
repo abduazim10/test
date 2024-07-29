75b7f215528dabbaa2d76e7ba5f3332feec47e8c
abduazim10 <abduazimit@gmail.com>
2024-07-29 18:49:57 +0500

    ass

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ let btn = document.getElementById('btn')
 let body = document.querySelector('body')
 
 btn.addEventListener( 'click' ,()=>{
-    localStorage.setItem('Login',inp.value)
+    sessionStorage.setItem("login",inp.value)
 })
